a855185d67c7b683df86d5d9cfff90694f814fd5
Thomas Steifler <steiflerthomas@gmail.com>
2024-06-06 16:17:08 +0200

    add method to check for valid moves

--- graph.js
+++ graph.js
@@ -1,3 +1,12 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @returns {boolean}
+ */
+function validMove(x, y) {
+    return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+}
+
 function knightMoves() {
     const moves = [
         [1, 2],
